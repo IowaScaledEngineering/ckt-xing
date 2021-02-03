3578a1eb222be7d287d59594931b806b22f77545
Nathan D. Holmes <maverick@drgw.net>
2021-02-03 15:18:14 -0700

    Initial somewhat working template

--- src/ckt-xing.c
+++ src/ckt-xing.c
@@ -23,34 +23,191 @@ LICENSE:
 
 *************************************************************************/
 
+#include <stdlib.h>
+#include <stdbool.h>
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
 #include <util/delay.h>
-#include <stdbool.h>
+
+#include "debouncer.h"
+#include "hardware.h"
+
+
+#define LIGHT_LEFT_ON   0x01
+#define LIGHT_RIGHT_ON  0x02
+#define LIGHT_CONST_ON  0x04
+#define LIGHT_RATE      6
+
+volatile uint8_t lights = 0;
+volatile uint8_t decisecs = 0;
+volatile uint8_t eventTriggers = 0;
+volatile uint8_t lightFlashCounter = 0;
+
+void activateLights()
+{
+	if (0 == lights)
+		lights = LIGHT_LEFT_ON | LIGHT_CONST_ON;
+}
+
+void deactivateLights()
+{
+	lights = 0;
+}
+
+
+
+#define EVENT_DO_INPUT_READ   0x01
+#define EVENT_CHANGE_FLASHER  0x02
 
 ISR(TIMER0_COMPA_vect) 
 {
+	static uint8_t counter = 0;
 	static uint8_t ticks = 0;
-	ticks++;
+
+	static uint8_t lightPWM = 0;
+	static uint8_t leftLightPWMSetting = 0;
+	static uint8_t rightLightPWMSetting = 0;
+	static uint8_t constLightPWMSetting = 0;
+	
+	static uint8_t lightFlashCounter = 0;
+
+	// Do the light PWM stuff first so there's no phase jitter that would be
+	// visually distracting
+	uint8_t tmp = 0;
+	
+	lightPWM = ((lightPWM + 1) & 0x3F);
+	
+	if (lightPWM < leftLightPWMSetting)
+		tmp |= _BV(PB2);
+	if (lightPWM < rightLightPWMSetting)
+		tmp |= _BV(PB1);
+	if (lightPWM < constLightPWMSetting)
+		tmp |= _BV(PB0);
+
+	PORTB = (PORTB & ~(_BV(PB2) | _BV(PB1) | _BV(PB0))) | tmp;
+
+	if (159 == ++counter) // Roughly 100Hz counter
+	{
+		counter = 0;
+
+		// Every 20mS, read inputs
+		if (ticks & 0x01)
+			eventTriggers |= EVENT_DO_INPUT_READ;
+
+		if (10 == ++ticks)
+		{
+			// 10Hz timer
+			ticks = 0;
+
+			// If the flashing lights aren't on, reset the flasher counter
+			if (0 == (lights & (LIGHT_LEFT_ON | LIGHT_RIGHT_ON)))
+			{
+				lightFlashCounter = 0;
+			}
+			else if (8 == ++lightFlashCounter)
+			{
+				// Else the flashing lights are on and 0.8s has elapsed, switch lights
+				lightFlashCounter = 0;
+				if (lights & LIGHT_LEFT_ON)
+					lights = (lights & ~(LIGHT_LEFT_ON)) | LIGHT_RIGHT_ON;
+				else if (lights & LIGHT_RIGHT_ON)
+					lights = (lights & ~(LIGHT_RIGHT_ON)) | LIGHT_LEFT_ON;
+			}
+		}
+		
+
+		// Recalculate light states
+		if (lights & LIGHT_LEFT_ON)
+		{
+			if (leftLightPWMSetting <= 0x3F)
+				leftLightPWMSetting += LIGHT_RATE;
+
+		} else {
+			if (leftLightPWMSetting > 0)
+				leftLightPWMSetting -= LIGHT_RATE;
+		}
+
+		// Do light things
+		if (lights & LIGHT_RIGHT_ON)
+		{
+			if (rightLightPWMSetting <= 0x3F)
+				rightLightPWMSetting += LIGHT_RATE;
+
+		} else {
+			if (rightLightPWMSetting > 0)
+				rightLightPWMSetting -= LIGHT_RATE;
+		}
+
+		// Do light things
+		if (lights & LIGHT_CONST_ON)
+		{
+			if (constLightPWMSetting <= 0x3F)
+				constLightPWMSetting += LIGHT_RATE;
+
+		} else {
+			if (constLightPWMSetting > 0)
+				constLightPWMSetting -= LIGHT_RATE;
+		}
+	}
+}
+
+inline void init_16kHz_timer0()
+{
+	TCNT0 = 0;
+	OCR0A = 63;                                   // Get as close to a 16kHz rate as we can
+
+	TCCR0A = _BV(WGM01);
+	TCCR0B = _BV(CS01);
+	TIMSK0 |= _BV(OCIE0A);
+}
+
+void init()
+{
+	init_gpio();
+	init_16kHz_timer0();
 }
 
 
 int main(void)
 {
+	DebounceState8_t confSwitches;
+	DebounceState8_t xingInputs;
+	
 	// Deal with watchdog first thing
-	MCUSR = 0;								// Clear reset status
+	MCUSR = 0;                       // Clear reset status
 	wdt_reset();                     // Reset the WDT, just in case it's still enabled over reset
 	wdt_enable(WDTO_1S);             // Enable it at a 1S timeout.
-
 	cli();
-
-
+	init();
+	
+	initDebounceState8(&confSwitches, getSwitchState());
+	initDebounceState8(&xingInputs, getInputState());
+	
 	sei();
 
 	while(1)
 	{
 		wdt_reset();
+
+		if (eventTriggers & EVENT_DO_INPUT_READ)
+		{
+			eventTriggers &= ~(EVENT_DO_INPUT_READ);
+			debounce8(getSwitchState(), &confSwitches);
+			debounce8(getInputState(), &xingInputs);
+		}
+
+		if (xingInputs.debounced_state & INPUT_XING_ACTIVE)
+		{
+			activateLights();
+			activateBell();
+		}
+		else
+		{
+			deactivateLights();
+			deactivateBell();
+		}
 	}
 }
 
---
+++ src/hardware.c
@@ -0,0 +1,99 @@
+#include <stdlib.h>
+#include <avr/io.h>
+#include <stdbool.h>
+
+void init_gpio()
+{
+	// PORTB Bit Assignment
+	// PORTB:7 - Main Gates
+	// PORTB:6 - 4Q Secondary Gates
+	// PORTB:5 - SCK (programming only)
+	// PORTB:4 - MISO (programming only)
+	// PORTB:3 - MOSI (programming only)
+	// PORTB:2 - Left Lamps
+	// PORTB:1 - Right Lamps
+	// PORTB:0 - Const Lamps
+	PORTB = 0x00;
+	DDRB = 0xFF;
+	
+	// PORTC Bit Assignment
+	// PORTC:7 - n/a
+	// PORTC:6 - /RESET pin, not used for I/O
+	// PORTC:5 - SW4 input (negative logic)
+	// PORTC:4 - Island Occupancy (positive logic)
+	// PORTC:3 - East Approach Occupancy (positive logic)
+	// PORTC:2 - West Approach Occupancy (positive logic)
+	// PORTC:1 - Active In (positive logic)
+	// PORTC:0 - Active Out (positive logic)
+	PORTC = 0xFE;  //  All pullups on
+	DDRC = 0x01;
+
+	// PORTD Bit Assignment
+	// PORTD:7 - SW3 input (negative logic)
+	// PORTD:6 - SW2 input (negative logic)
+	// PORTD:5 - SW1 input (negative logic)
+	// PORTD:4 - Bell enable (negative logic)
+	// PORTD:3 - SW8 input (negative logic)
+	// PORTD:2 - SW7 input (negative logic)
+	// PORTD:1 - SW6 input (negative logic)
+	// PORTD:0 - SW5 input (negative logic)
+	PORTD = 0xEF;  //  All pullups on
+	DDRD = 0x10;
+}
+
+
+uint8_t getSwitchState()
+{
+	uint8_t retval = 0;
+	retval = (PIND>>5) & 0x07;
+	retval |= (PIND<< 4) & 0xF0;
+	retval |= (PINC & 0x20)?0x10:0x00; // Get SW4 from Port C
+	return (~retval); // Invert since these are all negative logic (switch on = low)
+}
+
+uint8_t getInputState()
+{
+	uint8_t retval = 0;
+	retval = (PINC>>1) & 0x0F;
+	return retval;
+}
+
+void activateMainGates()
+{
+	PORTB |= _BV(PB7);
+}
+
+void deactivateMainGates()
+{
+	PORTB &= ~_BV(PB7);
+}
+
+void activateSecondaryGates()
+{
+	PORTB |= _BV(PB6);
+}
+
+void deactivateSecondaryGates()
+{
+	PORTB &= ~_BV(PB6);
+}
+
+void setCrossingActive()
+{
+	PORTC |= _BV(PC0);
+}
+
+void clearCrossingActive()
+{
+	PORTC &= ~_BV(PC0);
+}
+
+void activateBell()
+{
+	PORTD &= ~_BV(PD4);
+}
+
+void deactivateBell()
+{
+	PORTD |= _BV(PD4);
+}
